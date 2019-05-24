a02f088526b9cd30e7d67785c1eb2617eba7bad1
Demas <shdm2012@gmail.com>
2019-05-25 02:56:33 +0900

    Добавлен запрос на добавление данных в ActiveResp

--- it-start/offerScreen.cs
+++ it-start/offerScreen.cs
@@ -22,100 +22,91 @@ namespace it_start
         {
             int caseSwitch1 = comboBox1.SelectedIndex;
             int caseSwitch2 = comboBox2.SelectedIndex;
-            double aLat, aLon, bLat, bLon;
+            String aLat, aLon, bLat, bLon;
 
             switch (caseSwitch1)
             {
                 case 0: // "Кузнечная(Ленина)":
-                    aLat = 50.256451;
-                    aLon = 127.547085;
+                    aLat = "50.256451";
+                    aLon = "127.547085";
                     MessageBox.Show(aLat + ", " +aLon);
                     break;
                 case 1: // "Театральная(Зейская)":
-                    aLat = 50.259320;
-                    aLon = 127.550686;
+                    aLat = "50.259320";
+                    aLon = "127.550686";
                     MessageBox.Show(aLat + ", " + aLon);
                     break;
                 case 2: //"Шимановского(Горького)":
-                    aLat = 50.265439;
-                    aLon = 127.542868;
+                    aLat = "50.265439";
+                    aLon = "127.542868";
                     MessageBox.Show(aLat + ", " + aLon);
                     break;
                 case 3: //"Универмаг":
-                    aLat = 50.262894;
-                    aLon = 127.534690;
+                    aLat = "50.262894";
+                    aLon = "127.534690";
                     MessageBox.Show(aLat + ", " + aLon);
                     break;
                 case 4: //"Шимановского(Ленина)":
-                    aLat = 50.257093;
-                    aLon = 127.540564;
+                    aLat = "50.257093";
+                    aLon = "127.540564";
                     MessageBox.Show(aLat + ", " + aLon);
                     break;
                 default:
-                    MessageBox.Show("Default case");
+                    aLat = "0";
+                    aLon = "0";
                     break;
             }
 
             switch (caseSwitch2)
             {
                 case 0: // "Кузнечная(Ленина)":
-                    bLat = 50.256451;
-                    bLon = 127.547085;
+                    bLat = "50.256451";
+                    bLon = "127.547085";
                     MessageBox.Show(bLat + ", " + bLon);
                     break;
                 case 1: // "Театральная(Зейская)":
-                    bLat = 50.259320;
-                    bLon = 127.550686;
+                    bLat = "50.259320";
+                    bLon = "127.550686";
                     MessageBox.Show(bLat + ", " + bLon);
                     break;
                 case 2: //"Шимановского(Горького)":
-                    bLat = 50.265439;
-                    bLon = 127.542868;
+                    bLat = "50.265439";
+                    bLon = "127.542868";
                     MessageBox.Show(bLat + ", " + bLon);
                     break;
                 case 3: //"Универмаг":
-                    bLat = 50.262894;
-                    bLon = 127.534690;
+                    bLat = "50.262894";
+                    bLon = "127.534690";
                     MessageBox.Show(bLat + ", " + bLon);
                     break;
                 case 4: //"Шимановского(Ленина)":
-                    bLat = 50.257093;
-                    bLon = 127.540564;
-                    MessageBox.Show(bLat + ", " + bLon);
+                    bLat = "50.257093";
+                    bLon = "127.540564";
+                    MessageBox.Show(comboBox2.Text);
                     break;
                 default:
-                    MessageBox.Show("Default case");
+                    bLat = "0";
+                    bLon = "0";
                     break;
             }
 
-            //using (SQLiteConnection conn = new SQLiteConnection("Data Source=bus.db; Version=3;"))
-            //{
-            //    conn.Open();
-            //    SQLiteCommand cmd = conn.CreateCommand();
-            //    cmd.CommandText = "";//"INSERT INTO ActiveResp (Astop, Bstop, ALon, ALat, BLon, BLat) VALUES (‘London’, ‘Hoffman’, 2001)";
-            //    try
-            //    {
-            //        SQLiteDataReader r = cmd.ExecuteReader();
-            //        string line = String.Empty;
-            //        while (r.Read())
-            //        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=bus.db; Version=3;"))
+            {
+                conn.Open();
+                SQLiteCommand cmd = conn.CreateCommand();
+                
+                try
+                {
+                    cmd.CommandText = "INSERT INTO ActiveResp (Astop, Bstop, ALon, ALat, BLon, BLat) VALUES ('"+comboBox1.Text+"','"+comboBox2.Text+"',"+aLon+","+aLat+","+bLon+","+bLat+")";
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
 
-            //            line = r["id"].ToString() + ", "
-            //                 + r["username"].ToString() + ", "
-            //                 + r["Astop"].ToString() + ", "
-            //                 + r["ALat"].ToString() + ", "
-            //                 + r["BLon"].ToString();
-            //            MessageBox.Show(line);
-            //        }
-            //        r.Close();
-            //    }
-            //    catch (SQLiteException ex)
-            //    {
-            //        MessageBox.Show(ex.Message);
-            //    }
-
-            //    conn.Dispose();
-            //}
+                conn.Dispose();
+            }
         }
     }
 }
