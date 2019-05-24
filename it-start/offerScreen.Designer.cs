53568f0c5710cf24f0977172f92e5f38861f1305
Demas <shdm2012@gmail.com>
2019-05-25 01:11:05 +0900

    Add database

--- it-start/offerScreen.Designer.cs
+++ it-start/offerScreen.Designer.cs
@@ -39,27 +39,30 @@
             // 
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(72, 93);
+            this.comboBox1.Location = new System.Drawing.Point(96, 114);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(238, 39);
+            this.comboBox1.Size = new System.Drawing.Size(316, 47);
             this.comboBox1.TabIndex = 0;
             // 
             // comboBox2
             // 
             this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(72, 275);
+            this.comboBox2.Location = new System.Drawing.Point(96, 338);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(238, 39);
+            this.comboBox2.Size = new System.Drawing.Size(316, 47);
             this.comboBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(78, 50);
+            this.label1.Location = new System.Drawing.Point(104, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 31);
+            this.label1.Size = new System.Drawing.Size(132, 39);
             this.label1.TabIndex = 2;
             this.label1.Text = "Откуда";
             // 
@@ -67,33 +70,37 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(81, 219);
+            this.label2.Location = new System.Drawing.Point(108, 270);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 31);
+            this.label2.Size = new System.Drawing.Size(96, 39);
             this.label2.TabIndex = 3;
             this.label2.Text = "Куда";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(87, 392);
+            this.button1.Location = new System.Drawing.Point(116, 482);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(203, 46);
+            this.button1.Size = new System.Drawing.Size(271, 57);
             this.button1.TabIndex = 4;
             this.button1.Text = "Заказать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // offerScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "offerScreen";
-            this.Size = new System.Drawing.Size(370, 520);
+            this.Size = new System.Drawing.Size(493, 640);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- it-start/offerScreen.cs
+++ it-start/offerScreen.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace it_start
 {
@@ -15,6 +16,38 @@ namespace it_start
         public offerScreen()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=bus.db; Version=3;"))
+            {
+                conn.Open();
+                SQLiteCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM ActiveResp";
+                try
+                {
+                    SQLiteDataReader r = cmd.ExecuteReader();
+                    string line = String.Empty;
+                    while (r.Read())
+                    {
+                        
+                        line = r["id"].ToString() + ", "
+                             + r["username"].ToString() + ", "
+                             + r["Astop"].ToString() + ", "
+                             + r["Lat"].ToString();
+                        Console.WriteLine(line);
+                        MessageBox.Show("wowo");
+                    }
+                    r.Close();
+                }
+                catch (SQLiteException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                conn.Dispose();
+            }
         }
     }
 }
