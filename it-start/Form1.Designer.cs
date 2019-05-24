e2f359c6f556616d98233a39dc2040f51a7aafe8
Stepan S <dumn2011@gmail.com>
2019-05-25 00:31:30 +0900

    Added mainForm and offerScreen forms

--- it-start/Form1.Designer.cs
+++ it-start/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace it_start
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,20 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.sidePanel = new System.Windows.Forms.Panel();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.offerScreen1 = new it_start.offerScreen();
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // sidePanel
+            // 
+            this.sidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidePanel.Location = new System.Drawing.Point(0, 0);
+            this.sidePanel.Name = "sidePanel";
+            this.sidePanel.Size = new System.Drawing.Size(56, 520);
+            this.sidePanel.TabIndex = 0;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.offerScreen1);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(56, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(370, 520);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // offerScreen1
+            // 
+            this.offerScreen1.Location = new System.Drawing.Point(0, 0);
+            this.offerScreen1.Name = "offerScreen1";
+            this.offerScreen1.Size = new System.Drawing.Size(370, 520);
+            this.offerScreen1.TabIndex = 0;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(496, 520);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(426, 520);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.sidePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel sidePanel;
+        private System.Windows.Forms.Panel mainPanel;
+        private offerScreen offerScreen1;
     }
 }
 
