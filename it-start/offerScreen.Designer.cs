fa4b8214d041c55563ba7fed86df5dc534bd126a
Demas <shdm2012@gmail.com>
2019-05-26 00:49:51 +0900

    Добавлено уведомление

--- it-start/offerScreen.Designer.cs
+++ it-start/offerScreen.Designer.cs
@@ -122,11 +122,14 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(69, 290);
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(36, 304);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 13);
+            this.label4.Size = new System.Drawing.Size(311, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "Ваша заявка была добавлена";
+            this.label4.Visible = false;
             // 
             // offerScreen
             // 
