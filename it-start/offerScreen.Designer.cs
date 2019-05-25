778ec26b9cc488546e688b534d19595d83e6aee1
Demas <shdm2012@gmail.com>
2019-05-26 00:49:34 +0900

    Добавлено уведомление

--- it-start/offerScreen.Designer.cs
+++ it-start/offerScreen.Designer.cs
@@ -35,6 +35,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -47,10 +48,9 @@
             "Шимановского(Горького)",
             "Универмаг",
             "Шимановского(Ленина)"});
-            this.comboBox1.Location = new System.Drawing.Point(76, 71);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Location = new System.Drawing.Point(57, 58);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(367, 47);
+            this.comboBox1.Size = new System.Drawing.Size(276, 39);
             this.comboBox1.TabIndex = 0;
             // 
             // comboBox2
@@ -63,20 +63,18 @@
             "Шимановского(Горького)",
             "Универмаг",
             "Шимановского(Ленина)"});
-            this.comboBox2.Location = new System.Drawing.Point(76, 178);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Location = new System.Drawing.Point(57, 145);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(367, 47);
+            this.comboBox2.Size = new System.Drawing.Size(276, 39);
             this.comboBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(105, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(79, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 39);
+            this.label1.Size = new System.Drawing.Size(106, 31);
             this.label1.TabIndex = 2;
             this.label1.Text = "Откуда";
             // 
@@ -84,20 +82,18 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(109, 122);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(82, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 39);
+            this.label2.Size = new System.Drawing.Size(77, 31);
             this.label2.TabIndex = 3;
             this.label2.Text = "Куда";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(116, 482);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(87, 392);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(271, 57);
+            this.button1.Size = new System.Drawing.Size(203, 46);
             this.button1.TabIndex = 4;
             this.button1.Text = "Заказать";
             this.button1.UseVisualStyleBackColor = true;
@@ -107,27 +103,36 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(109, 227);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(82, 184);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(175, 39);
+            this.label3.Size = new System.Drawing.Size(140, 31);
             this.label3.TabIndex = 5;
             this.label3.Text = "Ваше имя";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
-            this.textBox1.Location = new System.Drawing.Point(76, 274);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(57, 223);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.MinimumSize = new System.Drawing.Size(4, 55);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(367, 55);
+            this.textBox1.Size = new System.Drawing.Size(276, 38);
             this.textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ваша заявка была добавлена";
             // 
             // offerScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
@@ -135,9 +140,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "offerScreen";
-            this.Size = new System.Drawing.Size(493, 640);
+            this.Size = new System.Drawing.Size(370, 520);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,5 +156,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
